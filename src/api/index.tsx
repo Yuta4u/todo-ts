ac8ad6b68c5db970ed43c1bbadb235b93c5581d6
Yuta4u <yudhistira4u@gmail.com>
2023-10-17 02:05:11 +0700

    handle cors

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,10 @@
 import axios from "axios"
 const url = "https://todo-ts-be-production.up.railway.app/v1/todos"
 
+const headers: any = {
+  "Content-Type": "application/json",
+}
+
 type TPostTodos = {
   title: string
   deskripsi: string
@@ -9,13 +13,34 @@ type TPostTodos = {
 }
 
 export function getTodos() {
-  return axios.get(url).then((res) => res.data.todos)
+  return axios
+    .get(url, { headers })
+    .then((response) => {
+      response.data.todos
+    })
+    .catch((error) => {
+      console.error("Error fetching users:", error)
+    })
 }
 
 export function postTodos(data: TPostTodos) {
-  return axios.post(url, data).then((res) => res.data)
+  return axios
+    .post(url, data, { headers })
+    .then((response) => {
+      console.log("Todos created:", response.data)
+    })
+    .catch((error) => {
+      console.error("Error creating user:", error)
+    })
 }
 
 export function deleteTodos(id: number) {
-  return axios.delete(`${url}/${id}`)
+  return axios
+    .delete(`${url}/${id}`, { headers })
+    .then((response) => {
+      console.log("Todos deleted:", response.data)
+    })
+    .catch((error) => {
+      console.error("Error deleting user:", error)
+    })
 }
